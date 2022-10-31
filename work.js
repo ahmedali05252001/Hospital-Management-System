6c0a33cc45ffbc503bebd961fbc06be91a6b0946
Redon <redoncapuni@gmail.com>
2022-10-31 19:11:55 +0300

    Update work.js

--- work.js
+++ work.js
@@ -1,3 +1,7 @@
 // hey this is a comment
 console.log("Hello World");
 console.log("Hello World"); // 2nd time
+
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
